2ad2afbabf8178fb725b29b9ff7547390afa6134
Clovis Carmezini <cloviscarmezini14@gmail.com>
2022-12-28 10:21:24 -0300

    first commit

--- dist/test/repositories/in-memory-notifications-repository.d.ts
+++
@@ -1,6 +0,0 @@
-import { Notification } from "@app/entities/notification";
-import { NotificationsRepository } from "@app/repositories/notifications-repository";
-export declare class InMemoryNotificationsRepository implements NotificationsRepository {
-    notifications: Notification[];
-    create(notification: Notification): Promise<void>;
-}
